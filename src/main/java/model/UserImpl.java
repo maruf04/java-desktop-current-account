7524e967695438a82bec7a0e8deb44f4d7a06f0f
Ömer Kırçal <omerkircal@outlook.com>
2022-04-14 16:45:53 +0300

    User Updated

--- src/main/java/model/UserImpl.java
+++ src/main/java/model/UserImpl.java
@@ -1,7 +1,119 @@
 package model;
 
 
+import props.User;
+import utils.DB;
+import utils.Util;
+import views.UserChangePassword;
+import views.UserPanel;
 
-public class UserImpl{
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Random;
 
+public class UserImpl implements IUser{
+    DB db=new DB();
+    public static String name="";
+    public static int uid=0;
+
+
+    public static int verificationCode = new Random().nextInt(900000) + 100000;
+
+    @Override
+    public boolean userLogin(String email, String password) {
+        boolean status=false;
+        try {
+            String sql="select * from user where email=? and password=?";
+            PreparedStatement pre=db.connect().prepareStatement(sql);
+            pre.setString(1,email);
+            pre.setString(2, Util.MD5(password));
+            ResultSet rs=pre.executeQuery();
+            status=rs.next();
+            if(status){
+                name=rs.getString("name")+" "+rs.getString("surname");
+                uid=rs.getInt("uid");
+            }
+        }catch (Exception e){
+            System.out.println("UserLogin Error: "+e);
+        }finally {
+            db.close();
+        }
+        return status;
+    }
+
+    @Override
+    public int userInsert(User user) {
+        int status=0;
+        try {
+            String sql="insert into user values(null,?,?,?,?)";
+            PreparedStatement pre=db.connect().prepareStatement(sql);
+            pre.setString(1,user.getName());
+            pre.setString(2,user.getSurname());
+            pre.setString(3,user.getEmail());
+            pre.setString(4,Util.MD5(user.getPassword()));
+            status=pre.executeUpdate();
+        }catch (Exception e){
+            System.out.println("UserInsert Error: "+e);
+        }finally {
+            db.close();
+        }
+        return status;
+    }
+
+    @Override
+    public int userUpdate(User user) {
+        int status=0;
+        try {
+            String sql="update user set name=?,  surname=?,  email=?,  password=? where uid=?";
+            PreparedStatement pre=db.connect().prepareStatement(sql);
+            pre.setString(1,user.getName());
+            pre.setString(2,user.getSurname());
+            pre.setString(3,user.getEmail());
+            pre.setString(4, Util.MD5(user.getPassword()));
+            pre.setInt(5, user.getUid());
+            status= pre.executeUpdate();
+        }catch (Exception e){
+            System.err.println("UserUpdate Error: "+ e);
+        }finally {
+            db.close();
+        }
+
+        return status;
+    }
+
+    @Override
+    public int userChangePassword(User user) {
+        int status=0;
+        try {
+            String sql="update user set password=? where email=?";
+            PreparedStatement pre=db.connect().prepareStatement(sql);
+            pre.setString(1,Util.MD5(user.getPassword()));
+            pre.setString(2,user.getEmail());
+            status=pre.executeUpdate();
+
+        }catch (Exception e){
+            System.out.println("userChangePasswordSendMail Error: "+e);
+        }finally {
+            db.close();
+        }
+        return status;
+    }
+
+    @Override
+    public int userForgetPassword(User user) {
+        int status=0;
+        try {
+            String sql="update user set password=? where email=?";
+            PreparedStatement pre=db.connect().prepareStatement(sql);
+            pre.setString(1,Util.MD5(user.getPassword()));
+            pre.setString(2,user.getEmail());
+            status=pre.executeUpdate();
+
+        }catch (Exception e){
+            System.out.println("userChangePasswordSendMail Error: "+e);
+        }finally {
+            db.close();
+        }
+        return status;
+    }
 }
---
+++ src/main/java/view/user/UserChangePassword.java
@@ -0,0 +1,146 @@
+/*
+ * Created by JFormDesigner on Thu Apr 14 01:19:47 TRT 2022
+ */
+
+package views;
+
+import model.UserImpl;
+import props.User;
+import utils.Util;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Locale;
+import javax.swing.*;
+import javax.swing.GroupLayout;
+
+/**
+ * @author unknown
+ */
+public class UserChangePassword extends JFrame {
+
+
+    UserImpl userImpl=new UserImpl();
+
+    public UserChangePassword() {
+        initComponents();
+        txtEmail.setText(UserPanel.emailChange);
+    }
+
+    private void thisWindowClosing(WindowEvent e) {
+        new UserPanel().setVisible(true);
+    }
+
+    private void btnChangePasswordClicked(ActionEvent e) {
+
+        User user=fncDataValid();
+
+        int answer=JOptionPane.showConfirmDialog(this,"Are you sure you want to update?","Update Window",JOptionPane.YES_OPTION);
+        if(answer==0){
+            if(txtverificationCode.equals(UserImpl.verificationCode)){
+                userImpl.userChangePassword(user);
+            }else{
+                lblError.setText("Verification Code is Wrong");
+            }
+        }else{
+            JOptionPane.showMessageDialog(this,"Please Choose!"); //this kendini burada ortala
+        }
+    }
+
+    public User fncDataValid(){
+
+        String email=txtEmail.getText().trim().toLowerCase(Locale.ROOT);
+        String verificationCode=txtverificationCode.getText().trim();
+        String password=txtPassword.getText();
+
+        if(email.equals("")){
+           lblError.setText("Please Enter Email");
+            txtEmail.requestFocus();
+        }else if(verificationCode.equals("")){
+            lblError.setText("Please Enter Verification Code");
+            txtverificationCode.requestFocus();
+        }else if(password.equals("")){
+            lblError.setText("Please Enter New Password");
+            txtPassword.requestFocus();
+        }else {
+            lblError.setText("");
+            User user=new User(email,password);
+
+            return user;
+        }
+        return null;
+    }
+
+
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        txtEmail = new JTextField();
+        txtverificationCode = new JTextField();
+        btnChangePassword = new JButton();
+        lblError = new JLabel();
+        txtPassword = new JTextField();
+
+        //======== this ========
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                thisWindowClosing(e);
+            }
+        });
+        Container contentPane = getContentPane();
+
+        //---- btnChangePassword ----
+        btnChangePassword.setText("CHANGE");
+        btnChangePassword.addActionListener(e -> btnChangePasswordClicked(e));
+
+        //---- lblError ----
+        lblError.setText(" ");
+
+        GroupLayout contentPaneLayout = new GroupLayout(contentPane);
+        contentPane.setLayout(contentPaneLayout);
+        contentPaneLayout.setHorizontalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGroup(contentPaneLayout.createSequentialGroup()
+                    .addGroup(contentPaneLayout.createParallelGroup()
+                        .addGroup(contentPaneLayout.createSequentialGroup()
+                            .addGap(145, 145, 145)
+                            .addComponent(btnChangePassword))
+                        .addGroup(contentPaneLayout.createSequentialGroup()
+                            .addGap(67, 67, 67)
+                            .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+                                .addComponent(txtEmail)
+                                .addComponent(lblError, GroupLayout.DEFAULT_SIZE, 325, Short.MAX_VALUE)
+                                .addComponent(txtverificationCode)
+                                .addComponent(txtPassword, GroupLayout.DEFAULT_SIZE, 325, Short.MAX_VALUE))))
+                    .addContainerGap(171, Short.MAX_VALUE))
+        );
+        contentPaneLayout.setVerticalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGroup(contentPaneLayout.createSequentialGroup()
+                    .addGap(28, 28, 28)
+                    .addComponent(txtEmail, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                    .addComponent(txtverificationCode, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                    .addGap(18, 18, 18)
+                    .addComponent(txtPassword, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 64, Short.MAX_VALUE)
+                    .addComponent(btnChangePassword)
+                    .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                    .addComponent(lblError)
+                    .addGap(19, 19, 19))
+        );
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    private JTextField txtEmail;
+    private JTextField txtverificationCode;
+    private JButton btnChangePassword;
+    private JLabel lblError;
+    private JTextField txtPassword;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
