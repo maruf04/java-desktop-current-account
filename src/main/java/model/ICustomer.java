65aac43f40cda3be15f7cce42430d33790239826
Ali İĞAN <aaigan.rsm@gmail.com>
2022-04-14 04:06:41 +0300

    update customer

--- src/main/java/model/ICustomer.java
+++ src/main/java/model/ICustomer.java
@@ -1,6 +1,18 @@
 package model;
 
 
+import props.Customer;
+
+import javax.swing.table.DefaultTableModel;
+import java.util.List;
+
 public interface ICustomer {
+    int customerInsert(Customer customer);
+    int customerDelete(int customerId);
+    int customerUpdate(Customer customer);
+    List<Customer> customerList();
+    List<Customer> customerSearch(String data);
+    DefaultTableModel customerTableModel();
+
 
 }
